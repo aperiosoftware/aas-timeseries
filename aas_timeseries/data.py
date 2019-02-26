ff5561f53e84c77303e306a711b71a828b7da69a
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-02-26 10:33:10 +0000

    Provide a way to include only the required data in the exported data, and tidied up API for saving/exporting

--- aas_timeseries/data.py
+++ aas_timeseries/data.py
@@ -1,8 +1,4 @@
 import uuid
-from io import StringIO
-
-from astropy.time import Time
-from astropy.table import Table
 
 
 class Data:
@@ -11,40 +7,3 @@ class Data:
         self.time_series = time_series
         self.uuid = str(uuid.uuid4())
         self.time_column = 'time'
-
-    def to_vega(self, embed_data=True):
-
-        table = Table()
-        table[self.time_column] = self.time_series.time.isot
-        for colname in self.time_series.colnames:
-            if colname != 'time':
-                table[colname] = self.time_series[colname]
-
-        parse = {}
-        for colname in table.colnames:
-            column = table[colname]
-            if colname == self.time_column:
-                parse[colname] = 'date'
-            elif column.dtype.kind in 'fi':
-                parse[colname] = 'number'
-            elif column.dtype.kind in 'b':
-                parse[colname] = 'boolean'
-            else:
-                parse[colname] = 'string'
-
-        vega = {'name': self.uuid,
-                'format': {'type': 'csv',
-                           'parse': parse}}
-
-        if embed_data:
-            s = StringIO()
-            table.write(s, format='ascii.basic', delimiter=',')
-            s.seek(0)
-            csv_string = s.read()
-            vega['values'] = csv_string
-        else:
-            filename = self.uuid + '.csv'
-            table.write(filename, format='ascii.basic', delimiter=',')
-            vega['url'] = filename
-
-        return vega
