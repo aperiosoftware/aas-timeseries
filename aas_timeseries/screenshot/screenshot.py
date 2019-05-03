f5cbb4b88cd0e926e9c5f1dd7c3f2dba5251f8c8
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-05-03 10:47:42 +0100

    Now properly wait for figure to be initialized when doing screenshot, but still need to wait a bit longer to be sure

--- aas_timeseries/screenshot/screenshot.py
+++ aas_timeseries/screenshot/screenshot.py
@@ -4,7 +4,6 @@
 # of the widget with Qt.
 
 import os
-import json
 import shutil
 import time
 import tempfile
@@ -17,6 +16,8 @@ from aas_timeseries.screenshot.qt_web_widget import get_qt_web_widget
 __all__ = ['interactive_screenshot']
 
 ROOT = os.path.dirname(__file__)
+TIMEOUT = 60  # seconds
+WAIT_TIME = 2  # seconds
 
 
 def interactive_screenshot(json_filename, prefix):
@@ -44,22 +45,40 @@ def interactive_screenshot(json_filename, prefix):
     web.resize(600, 400)
     web.show()
 
+    # Find the views that are present in the figure
+
     start = time.time()
-    while time.time() - start < 2:
+    while time.time() - start < TIMEOUT:
+        app.processEvents()
+        ready = page.runJavaScript('(typeof(figure_ready) != "undefined") && figure_ready == 1;', asynchronous=False)
+        if ready:
+            break
+    else:
+        raise ValueError("Timed out while waiting for interactive figure to open")
+
+    # Temporary: at the moment it seems the figure isn't quite ready for real
+    # (https://github.com/aperiosoftware/timeseries.js/issues/65) so just wait
+    # a little longer
+    start = time.time()
+    while time.time() - start < WAIT_TIME:
         app.processEvents()
 
     web.save_to_file(prefix + '.png')
 
     # Find the views that are present in the figure
-    views = page.runJavaScript('ex1.getViews();', asynchronous=False)
+    views = page.runJavaScript('figure.getViews();', asynchronous=False)
 
     if len(views) > 1:
         for view_index in range(1, len(views)):
 
-            page.runJavaScript('ex1.setView({0});'.format(view_index), asynchronous=False)
+            page.runJavaScript('figure.setView({0});'.format(view_index), asynchronous=False)
+
+            # There is currently no way to know if the view has been actually
+            # refreshed (https://github.com/aperiosoftware/timeseries.js/issues/64)
+            # so just wait a little.
 
             start = time.time()
-            while time.time() - start < 2:
+            while time.time() - start < WAIT_TIME:
                 app.processEvents()
 
             web.save_to_file(prefix + '_view{0}.png'.format(view_index))
